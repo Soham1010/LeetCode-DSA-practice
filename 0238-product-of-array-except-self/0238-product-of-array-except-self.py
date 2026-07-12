c22a7e7cd5ff1defa6f538e98ddbdc24a57889e2
soham mehra <78025707+Soham1010@users.noreply.github.com>
2026-07-11 20:14:22 -0400

    Time: 28 ms (38.98%), Space: 25.3 MB (81.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        out = [1] * n  # Initialize with 1s to make multiplication seamless
+        out = [0] * n
 
-        # Step 1: Calculate Prefix Products
-        # out[i] will store the product of all elements to the left of i
-        prefix = 1
+        prod = 1
+        post = nums[n-1]
+        # presum
         for i in range(n):
-            out[i] = prefix
-            prefix *= nums[i]
-
-        # Step 2: Calculate Postfix Products on the fly
-        # Multiply out[i] by the product of all elements to the right of i
-        postfix = 1
-        for i in range(n - 1, -1, -1):
-            out[i] *= postfix
-            postfix *= nums[i]
-
+            if i == 0:
+                out[0] = nums[0]
+            else:
+                out[i] = out[i-1] * nums[i]
+        
+        # postsum
+        for i in range(n-1, -1, -1):
+            # Extremes pe 1
+            # else index, prefix-1, postfix+1
+            if (i==0) :
+                out[0] = 1 * post
+            elif (i==n-1) :
+                out[n-1] = out[i-1] * 1 
+            else:
+                # Normally
+                out[i] = out[i-1] * post
+                post *= nums[i]
+        
         return out
+                
