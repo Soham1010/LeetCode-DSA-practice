bd3295f6007da8305bcc4751406510784bedd407
soham mehra <78025707+Soham1010@users.noreply.github.com>
2026-07-20 17:48:46 -0400

    Time: 3 ms (60.85%), Space: 21 MB (93.17%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -3,20 +3,10 @@ from collections import defaultdict
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         """
-        1. Create a H.Map
-        2. Store elements -> occurances
-        3. Another pass to check occurances
+        Efficient Bit manupulation
         """
-
-        hmap = defaultdict(int)
-        length = len(nums)
-
+        result = 0
         for i in nums:
-            hmap[i] += 1
-
-        for i in nums:
-            if hmap[i] <2:
-                return i
-        return -1
-
-        
+            result ^= i
+        return result
+            
